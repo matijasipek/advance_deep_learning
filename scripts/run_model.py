f574e19fc3e44244d1208a0b8707beef692f83a1
matijasipek <matija.siipek@hotmail.com>
2024-04-03 12:06:15 +0200

    stuck on tkinter installation, this is a checkpoint before messing up the envirnoment

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -1,4 +1,12 @@
-import math
+import argparse, json, os
+from imageio import imwrite
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import sys
+import os
+
+project_root = '/zhome/48/2/181238/cv_project/advance_deep_learning'
+sys.path.append(project_root)
+
+from sg2im.model import Sg2ImModel
+from sg2im.data.utils import imagenet_deprocess_batch
+import sg2im.vis as vis
--- test.py
+++
@@ -1,5 +0,0 @@
-print('aaabbb')
-
-print("dan")
-
-print('test2')
